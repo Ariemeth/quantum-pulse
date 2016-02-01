23dfcb558bde8a9bea5c722153a6a6d4f7b3a68c
Edward Carmack <AriemethAcc@gmail.com>
2016-01-31 21:10:59 -0500

    simple code organization changes

--- main.go
+++ main.go
@@ -15,43 +15,18 @@ const windowHeight = 600
 func init() {
 	// GLFW event handling must run on the main OS thread
 	runtime.LockOSThread()
-	log.Print("OS thread locked")
 }
 
 func main() {
+
 	if err := glfw.Init(); err != nil {
 		log.Fatalln("failed to initialize glfw:", err)
 	}
 	defer glfw.Terminate()
 
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-	window, err := glfw.CreateWindow(windowWidth, windowHeight, "Frame Assault", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-	window.MakeContextCurrent()
-	window.SetKeyCallback(onKey)
-	window.SetMouseButtonCallback(onMouseButton)
-	window.SetCloseCallback(onClose)
-	window.SetScrollCallback(onScroll)
-	window.SetCursorPosCallback(onCursorPos)
+	window := createWindow()
 
-	// Initialize Glow
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	fmt.Println("OpenGL version", version)
-
-	// Configure global settings
-	gl.Enable(gl.DEPTH_TEST)
-	gl.DepthFunc(gl.LESS)
-	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
+	initGL()
 
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
@@ -65,6 +40,47 @@ func main() {
 		glfw.PollEvents()
 	}
 
+}
+
+func createWindow() *glfw.Window {
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, "Frame Assault", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+	window.SetKeyCallback(onKey)
+	window.SetMouseButtonCallback(onMouseButton)
+	window.SetCloseCallback(onClose)
+	window.SetScrollCallback(onScroll)
+	window.SetCursorPosCallback(onCursorPos)
+
+	return window
+}
+
+func initGL() string {
+	// Initialize Glow
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	fmt.Println("OpenGL version", version)
+	fmt.Println("OpenGl shading version", gl.GoStr(gl.GetString(gl.SHADING_LANGUAGE_VERSION)))
+	fmt.Println("OpenGl renderer", gl.GoStr(gl.GetString(gl.RENDERER)))
+
+	// Configure global settings
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
+
+	return version
 }
 
 func onKey(window *glfw.Window, k glfw.Key, s int, action glfw.Action, mods glfw.ModifierKey) {
@@ -87,6 +103,8 @@ func onMouseButton(window *glfw.Window, b glfw.MouseButton, action glfw.Action, 
 
 	switch glfw.MouseButton(b) {
 	case glfw.MouseButtonLeft:
+		return
+	case glfw.MouseButtonRight:
 		return
 	default:
 		return
