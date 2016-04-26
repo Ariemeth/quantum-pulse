d12c738e403777ed757f4133a28c05d8414835a3
Edward Carmack <AriemethAcc@gmail.com>
2016-04-26 09:01:09 -0400

    Stubbed in camera component

--- engine/components/camera.go
+++ engine/components/camera.go
@@ -1,3 +1,27 @@
 package components
 
 //TODO: move project and view handling here.
+const (
+	// ComponentTypeCamera represents a Camera component's type.
+	ComponentTypeCamera = "Camera"
+)
+
+// Camera represents the Camera of an entity.
+type Camera interface {
+	Component
+}
+
+type camera struct {
+}
+
+// NewCamera creates a new Camera component.
+func NewCamera() Camera {
+	c := camera{}
+
+	return &c
+}
+
+// ComponentType retrieves the type name of this component.
+func (c *camera) ComponentType() string {
+	return ComponentTypeCamera
+}
