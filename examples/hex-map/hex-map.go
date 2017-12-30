208203b9284bacd10f64488d5f4a026337c36b66
Edward Carmack <ariemethacc@gmail.com>
2017-12-29 19:28:03 -0500

    moved window params to const values in hex map example

--- examples/hex-map/hex-map.go
+++ examples/hex-map/hex-map.go
@@ -6,11 +6,17 @@ import (
 	"github.com/Ariemeth/quantum-pulse/engine"
 )
 
+const (
+	screenWidth  = 800
+	screenHeight = 600
+	windowTitle  = "hex map test"
+)
+
 func main() {
 
 	e := engine.Engine{}
 
-	e.Init(800, 600,"hex map test")
+	e.Init(screenWidth, screenHeight, windowTitle)
 
 	sceneID, err := e.LoadSceneFile("scene1.json")
 	if err != nil {
